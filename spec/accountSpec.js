6b2d813a8026daee5e669b3eafa8b342d7cb9f71
hybridbad <hybridbad90@gmail.com>
2019-05-07 18:03:11 +0100

    Added conditionals for withdraw and deposit
    Test drive withdraw error throw when balance is less than withdraw amount
    Test drive exit deposit block when amount is negative

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,6 +1,7 @@
 
 describe('#feature test - deposit money into account',function () {
   let account;
+
   beforeEach(function(){
     account = new Account('James');
   })
@@ -14,16 +15,24 @@ describe('#feature test - deposit money into account',function () {
     expect(account.balance).toBe(0)
   })
 
+  it('deposit function defined', function(){
+    expect(account.deposit).toBeDefined();
+  })
+
   it('deposit some money into account', function(){
     account.deposit(100);
     expect(account.balance).toBe(100);
   })
 
-  it('depositing some money returns amount and timestamp in array', function(){
-    expect(account.deposit(100, '07-05-2019')).toEqual([100, '07-05-2019']);
-    expect(account.balance).toBe(100);
+  it('exit code block if amount less than 0', function(){
+    account.deposit(-1)
+    expect(account.balance).toBe(0)
   })
 
+  it('depositing some money is recorded in statement', function(){
+    account.deposit(100, '07-05-2019')
+    expect(account.statement[0]).toEqual({credit: 100, date: '07-05-2019'});
+  })
 
 })
 
@@ -31,30 +40,55 @@ describe('#feature test - withdraw money from account', function(){
   let account;
   beforeEach(function(){
     account = new Account('James');
-    account.deposit(1000, '07-05-2019');
+  })
+
+  it('withdraw function defined', function(){
+    expect(account.withdraw).toBeDefined();
   })
 
   it('withdraw some money from the account', function(){
+    account.deposit(1000, '07-05-2019');
     account.withdraw(100);
     expect(account.balance).toBe(900);
   })
 
-  it('withdraw some money returns amount and timestamp in array', function(){
-    expect(account.withdraw(100, '07-05-2019')).toEqual([-100, '07-05-2019']);
+  it('throws error when not enough money', function(){
+    account.deposit(5, '07-05-2019');
+    expect(() => {
+      account.withdraw(10, '07-05-2019');
+    }).toThrow();
+  })
+
+  it('withdraw some money and record object in statement', function(){
+    account.deposit(200)
+    account.withdraw(100, '07-05-2019')
+    expect(account.statement[1]).toEqual({debit: 100, date: '07-05-2019'})
   })
 
 })
 
 // describe('#feature test - print a bank statement', function(){
+
 //   let account;
 //   beforeEach(function(){
+//     spyOn(window.console, 'log');
 //     account = new Account('James');
 //     account.deposit(1000, '07-05-2019');
 //     account.withdraw(100, '07-05-2019');
 //   })
 
-//   it('prints a bank statement', function(){
-//     expect(account).to
+//   it('print statement function is defined', function(){
+//     expect(account.printStatement).toBeDefined();
 //   })
+
+//   it('statement contains deposit and withdraw objects', function(){
+    
+//   })
+
+//   it('prints a bank statement header', function(){
+//     account.printStatement();
+//     expect(window.console.log).toHaveBeenCalled();
+//   })
+
 
 // })
--- src/account.js
+++ src/account.js
@@ -3,16 +3,26 @@ class Account {
   constructor(name) {
     this.name = name;
     this.balance = 0;
+    this.statement = [];
   }
 
   deposit(amount, date){
+    if (amount < 0) return
+    const depositObj = {credit: amount, date: date};
     this.balance += amount;
-    return [amount, date]
+    this.statement.push(depositObj);
   }
 
   withdraw(amount, date){
+    if (amount > this.balance) throw "Not enough funds";
+    const withdrawObj = {debit: amount, date: date};
     this.balance -= amount;
-    return [-amount, date]
+    this.statement.push(withdrawObj);
   }
 
+  printStatement(){
+    console.log("date || credit || debit || balance")
+
+  }
 }
+
